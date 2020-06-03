d2666ab77f6bad4205e041f800f1426986372620
khantkyawkhaung <khantkyawkhaung288@gmail.com>
2020-06-04 00:14:26 +0630

    :construction: CLI-control features

---
+++ mpv-play.c
@@ -0,0 +1,153 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+
+#else
+#include <unistd.h>
+#include <libgen.h>
+#endif
+
+#include <mpv/client.h>
+
+
+#define COMMAND_NONE  0
+#define COMMAND_PAUSE 1
+#define COMMAND_MOVE  2
+#define COMMAND_QUIT  3
+
+
+static char exec_path[128];
+
+static void get_exec_path(char* buff, size_t len) {
+    #ifdef _WIN32
+    
+    #else
+    if (readlink("/proc/self/exe", buff, len) != -1)
+        dirname(buff);
+    else
+        buff[0] = '\0';
+    #endif
+}
+
+static int move_time = 0;
+
+static int read_command() {
+    char cmd_file[128+8];
+    sprintf(cmd_file, "%s/command", exec_path);
+    FILE *fp = fopen(cmd_file, "r");
+    if(fp == NULL)
+        return COMMAND_NONE;
+    
+    char cmd[64];
+    int i;
+    i = 0;
+    char ch = getc(fp);
+    while(ch != EOF) {
+        cmd[i] = ch;
+        ch = getc(fp);
+        i++;
+    }
+    cmd[i] = '\0';
+    fclose(fp);
+    remove(cmd_file);
+    
+    char *tokens[5];
+    char *tok = strtok(cmd, " ");
+    tokens[0] = tok;
+    i = 0;
+    while(tok != NULL) {
+        tok = strtok(NULL, " ");
+        tokens[++i] = tok;
+    }
+    
+    if(strcmp(tokens[0], "pause") == 0) {
+        return COMMAND_PAUSE;
+    }
+    else if(strcmp(tokens[0], "move") == 0) {
+        move_time = atoi(tokens[1]);
+        return COMMAND_MOVE;
+    }
+    else if(strcmp(tokens[0], "quit") == 0) {
+        return COMMAND_QUIT;
+    }
+    else {
+        return COMMAND_NONE;
+    }
+}
+
+static void check_error(int status) {
+    if(status < 0) {
+        printf("MPV API error: %s\n", mpv_error_string(status));
+        exit(1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        printf("Pass a single media file as argument\n");
+        return 1;
+    }
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    if(fp == NULL) {
+        printf("Media file does not exist.\n");
+        return 1;
+    }
+    else
+        fclose(fp);
+    
+    get_exec_path(exec_path, 128);
+    
+    mpv_handle *ctx = mpv_create();
+    if(!ctx) {
+        printf("Failed creating context\n");
+        return 1;
+    }
+    
+    check_error(mpv_set_option_string(ctx, "fs", "yes"));
+    check_error(mpv_set_option_string(ctx, "hwdec", "auto"));
+    check_error(mpv_set_option_string(ctx, "input-default-bindings", "yes"));
+    check_error(mpv_set_option_string(ctx, "input-vo-keyboard", "yes"));
+    int val = 1;
+    check_error(mpv_set_option(ctx, "osc", MPV_FORMAT_FLAG, &val));
+    check_error(mpv_initialize(ctx));
+    
+    // Play this file.
+    const char *play_cmd[] = {"loadfile", argv[1], NULL};
+    check_error(mpv_command(ctx, play_cmd));
+    
+    // Let it play, and wait until the user quits.
+    char play_indicator[128+5];
+    sprintf(play_indicator, "%s/play", exec_path);
+    fp = fopen(play_indicator, "w");
+    fprintf(fp, "play");
+    fclose(fp);
+    while(1) {
+        mpv_event *event = mpv_wait_event(ctx, 0.1);
+        if(event->event_id == MPV_EVENT_SHUTDOWN)
+            break;
+        
+        int cmd = read_command();
+        if(cmd == COMMAND_PAUSE) {
+            const char *cmd[] = {"keypress", "p", NULL};
+            check_error(mpv_command(ctx, cmd));
+        }
+        else if(cmd == COMMAND_MOVE) {
+            char buff[6];
+            sprintf(buff, "%d", move_time);
+            const char *cmd[] = {"seek", buff, NULL};
+            check_error(mpv_command(ctx, cmd));
+        }
+        else if(cmd == COMMAND_QUIT) {
+            const char *cmd[] = {"quit", NULL};
+            check_error(mpv_command(ctx, cmd));
+        }
+    }
+    
+    remove(play_indicator);
+    mpv_terminate_destroy(ctx);
+    return 0;
+}
---
+++ mpv-remote.c
@@ -0,0 +1,102 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#define sleep(X) Sleep(X)
+#else
+#include <unistd.h>
+#include <libgen.h>
+#define sleep(X) usleep((X)*1000)
+#endif
+
+
+static char exec_path[128];
+
+static void get_exec_path(char* buff, size_t len) {
+    #ifdef _WIN32
+    
+    #else
+    if (readlink("/proc/self/exe", buff, len) != -1)
+        dirname(buff);
+    else
+        buff[0] = '\0';
+    #endif
+}
+
+static void write_command(char* cmd) {
+    char cmd_file[128+8];
+    sprintf(cmd_file, "%s/command", exec_path);
+    FILE *fp = fopen(cmd_file, "w");
+    fprintf(fp, "%s", cmd);
+    fclose(fp);
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        return 1;
+    }
+    
+    get_exec_path(exec_path, 128);
+    
+    if(strcmp(argv[1], "-p") == 0) {
+        write_command("pause");
+    }
+    else if(strcmp(argv[1], "-m") == 0) {
+        if(argc != 3) {
+            printf("Need to specify time to move in seconds.\n");
+            return 1;
+        }
+        char buff[12];
+        sprintf(buff, "move %s", argv[2]);
+        write_command(buff);
+    }
+    else if(strcmp(argv[1], "-k") == 0) {
+        write_command("quit");
+    }
+    else {
+        FILE *fp;
+        fp = fopen(argv[1], "r");
+        if(fp == NULL) {
+            printf("Media file does not exist.\n");
+            return 1;
+        }
+        else
+            fclose(fp);
+        
+        char play_indicator[128+5];
+        sprintf(play_indicator, "%s/play", exec_path);
+        fp = fopen(play_indicator, "r");
+        if(fp != NULL) {
+            fclose(fp);
+            write_command("quit");
+            
+            int i=0;
+            while(1) {
+                sleep(100);
+                fp = fopen(play_indicator, "r");
+                if(fp == NULL)
+                    break;
+                fclose(fp);
+                
+                if(i == 10)
+                    break;
+                i++;
+            }
+            
+            write_command("none");
+        }
+        
+        char sys_cmd[256];
+        #ifdef _WIN32
+        sprintf(sys_cmd, "start %s/mpv-play %s", exec_path, argv[1]);
+        #else
+        sprintf(sys_cmd, "%s/mpv-play %s &", exec_path, argv[1]);
+        #endif
+        system(sys_cmd);
+    }
+    
+    return 0;
+}
